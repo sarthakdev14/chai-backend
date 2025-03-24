93e35b953b411f0f71cc8f13e0239d89c36fb29f
sarthakdev14 <aries.sarthakgupta@gmail.com>
2025-03-25 01:06:46 +0530

    setup mongodb

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./Constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: './.env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+//method 1 
+/*import express from "express";
+const app = express()
+(async() =>{
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URI}`/$,{DB_NAME}) 
+    app.on("error",(error)=>{
+      console.log("ERRR :" , error);
+      throw error
+    })
+    app.listen(process.env.PORT,()=>{
+      console.log(`app is listening on port ${process.env.port}`);
+    })
+
+  }catch(error){
+   console.error("ERROR :" , error)
+  }
+})()*/
